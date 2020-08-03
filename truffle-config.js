15f3008a6327028c522759ac2984012c5aaa449b
Wright <calebogundiya@gmail.com>
2020-08-03 16:29:31 +0100

    Setup: Project keys

--- truffle-config.js
+++ truffle-config.js
@@ -21,7 +21,7 @@
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
 const HDWalletProvider = require('truffle-hdwallet-provider');
 
-const infuraKey = "5e7c85bd4bad48e79ff79197ad46b83f";
+const infuraKey = "304ed06f11c348e2b05c1dbc9e6a2c38";
 
 const fs = require('fs');
 const mnemonic = fs.readFileSync(".secret").toString().trim();
@@ -50,9 +50,10 @@ module.exports = {
       network_id: "*", // Any network (default: none)
     },
     rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/304ed06f11c348e2b05c1dbc9e6a2c38`),
-      network_id: 3, // Ropsten's id
-      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4, // Rinkeby's id
+      gas: 4500000, // Rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000,
       confirmations: 2, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
